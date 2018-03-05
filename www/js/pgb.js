3ab6c8f03b1922c355f2a95c4c4a16769b432048
s-115-11 <s-115-11@gumiforest.ae.krakow.pl>
2018-03-05 17:48:31 +0100

    lekcja 5 test

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -44,3 +44,14 @@ function checkConnection() {
 
     alert('Connection type: ' + states[networkState]);
 }
+
+function onSuccess(acceleration) {
+    alert('Acceleration X: ' + acceleration.x + '\n' +
+          'Acceleration Y: ' + acceleration.y + '\n' +
+          'Acceleration Z: ' + acceleration.z + '\n' +
+          'Timestamp: '      + acceleration.timestamp + '\n');
+}
+
+function onError() {
+    alert('onError!');
+}
