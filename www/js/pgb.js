552d1e48422d005b2d6e97c2f6f275f57367afd4
toxwow <tomasz.tomzynski@gmail.com>
2018-03-11 18:36:53 +0100

    test kontakt11112231213

--- www/js/pgb.js
+++ www/js/pgb.js
@@ -46,16 +46,20 @@ function checkConnection() {
     alert('Connection type: ' + states[networkState]);
 }
 
-function onSuccess(acceleration) {
+function onSuccess1(acceleration) {
     alert('Acceleration X: ' + acceleration.x + '\n' +
           'Acceleration Y: ' + acceleration.y + '\n' +
           'Acceleration Z: ' + acceleration.z + '\n' +
           'Timestamp: '      + acceleration.timestamp + '\n');
 }
 
-function onError() {
+function onSuccess(contacts) {
+    alert('Found ' + contacts.length + ' contacts.');
+};
+
+function onError(contactError) {
     alert('onError!');
-}
+};
 
 var options      = new ContactFindOptions();
 options.filter   = "Bob";
